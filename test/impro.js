papandreou/impro

commit edbe2971362112001573512079bd85a949f7c714

Extend the sharp engine so it can coalesces resize/crop into one.

src/engines/sharp.js
@@ -6,6 +6,12 @@ function isNumberWithin(num, min, max) {
     return typeof num === 'number' && num >= min && num <= max;
 }
 
+function locatePreviousCommand(operations, nameToFind) {
+    return operations.find(operation => {
+        return operation.name === nameToFind;
+    });
+}
+
 var maxDimension = 16384;
 
 module.exports = {
@@ -64,22 +70,35 @@ module.exports = {
         if (options.sequentialRead) {
             sharpInstance = sharpInstance.sequentialRead();
         }
+
+        var operationsForExecution = [];
         operations.forEach(function (operation) {
+            var name = operation.name;
             var args = operation.args;
             if (operation.name === 'resize' && typeof pipeline.options.maxOutputPixels === 'number' && args[0] * args[1] > pipeline.options.maxOutputPixels) {
                 throw new errors.OutputDimensionsExceeded('resize: Target dimensions of ' + args[0] + 'x' + args[1] + ' exceed maxOutputPixels (' + pipeline.options.maxOutputPixels + ')');
             }
             // in sharp crop is implemented as options to resize
             if (operation.name === 'crop') {
-                operation.name = 'resize';
-                args = [ { fit: 'cover', gravity: operation.args[0] } ];
+                name = 'resize';
+                args = [ null, null, { fit: 'cover', position: args[0] } ];
+
+                const locatedOperation = locatePreviousCommand(operationsForExecution, name);
+                if (locatedOperation) {
+                    locatedOperation.args[2] = args[2];
+                    return;
+                }
             }
             // Compensate for https://github.com/lovell/sharp/issues/276
-            if (operation.name === 'extract' && args.length >= 4) {
+            if (name === 'extract' && args.length >= 4) {
                 args = [ { left: args[0], top: args[1], width: args[2], height: args[3] } ];
             }
-            return sharpInstance[operation.name](...args);
+
+            operationsForExecution.push({ name, args });
         });
+
+        operationsForExecution.map(({ name, args }) => sharpInstance[name](...args));
+
         pipeline.add(sharpInstance);
     }
 };

commit 58e8054874e83cdd96e2a244e87983ffafd7e75f

Exercise the resize/crop combining code for the sharp engine.
Add a test that thoroughly checks this code. While here also fix an
issue introduced by commit edbe297 whereby the original input ops
were altered rather than internal copies used by the sharp engine.

src/engines/sharp.js
@@ -7,7 +7,7 @@ function isNumberWithin(num, min, max) {
 }
 
 function locatePreviousCommand(operations, nameToFind) {
-    return operations.find(operation => {
+    return operations.findIndex(operation => {
         return operation.name === nameToFind;
     });
 }
@@ -176,12 +176,17 @@ module.exports = {
                 name = 'resize';
                 args = [null, null, { fit: 'cover', position: args[0] }];
 
-                const locatedOperation = locatePreviousCommand(
+                const locatedIndex = locatePreviousCommand(
                     operationsForExecution,
                     name
                 );
-                if (locatedOperation) {
-                    locatedOperation.args[2] = args[2];
+                if (locatedIndex > -1) {
+                    let locatedOperation = operationsForExecution[locatedIndex];
+                    locatedOperation = {
+                        ...locatedOperation,
+                        args: locatedOperation.args.concat(args[2])
+                    };
+                    operationsForExecution[locatedIndex] = locatedOperation;
                     return;
                 }
             }
@@ -205,5 +210,7 @@ module.exports = {
         );
 
         pipeline._attach(sharpInstance);
+
+        return operationsForExecution;
     }
 };

test/impro.js
@@ -444,6 +444,46 @@ describe('impro', function() {
                     .finally(() => cacheSpy.restore());
             })
         );
+
+        it('should combine resize with crop', () => {
+            const executeSpy = sinon.spy(impro.engineByName.sharp, 'execute');
+
+            const usedEngines = impro
+                .type('jpg')
+                .resize(10, 10)
+                .crop('northwest')
+                .flush().usedEngines;
+
+            return expect(executeSpy.returnValues[0], 'to equal', [
+                {
+                    name: 'resize',
+                    args: [10, 10, { fit: 'cover', position: 'northwest' }]
+                }
+            ])
+                .then(() => {
+                    // check that the external representation is unchanged
+                    return expect(usedEngines, 'to satisfy', [
+                        {
+                            name: 'sharp',
+                            operations: expect.it('to equal', [
+                                {
+                                    name: 'resize',
+                                    args: [10, 10],
+                                    engineName: 'sharp'
+                                },
+                                {
+                                    name: 'crop',
+                                    args: ['northwest'],
+                                    engineName: 'sharp'
+                                }
+                            ])
+                        }
+                    ]);
+                })
+                .finally(() => {
+                    executeSpy.restore();
+                });
+        });
     });
 
     describe('with the gifsicle engine', function() {