85376d97c5b255f92ff813336a51338349af9a2c
Alex J Burke <alex@alexjeffburke.com>
2019-11-16 12:44:24 +0100

    Pull in eslint-plugin-mocha and enforce basic test rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     ecmaVersion: 2018
   },
   extends: ['standard', 'prettier', 'prettier/standard'],
+  plugins: ['mocha'],
   env: {
     mocha: true
+  },
+  rules: {
+    'mocha/no-exclusive-tests': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-identical-title': 'error'
   }
 };
