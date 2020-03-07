8b445555249bbf8134a595cbae4c037bb92bd8da
Alex J Burke <alex@alexjeffburke.com>
2020-03-08 00:03:11 +0100

    Bring in the legacy query string parsing as a queryString function.
    The legacy parser is closely coupled to the impro parser as internally
    it functions as a preparation step which is then handed to the impro
    query string parser.
    
    Import it so it becomes another function exposed by the queryString
    file - this allows any project that exposes the legacy format to get
    rid of any previous dependencies and instead use impro alone.

--- src/queryString.js
+++ src/queryString.js
@@ -77,4 +77,106 @@ function parseImproQueryString(improInstance, queryString, allowOperation) {
   };
 }
 
+const resizeOptions = {
+  ignoreAspectRatio: true,
+  withoutEnlargement: true
+};
+
+function makeEngineAndArgsRegex(engineNames) {
+  return new RegExp(`^(${engineNames.join('|')})(?:=(.*))?`);
+}
+
+function prepareLegacyQueryString(improInstance, queryString) {
+  const engineNames = Object.keys(improInstance.engineByName);
+  const queryStringEngineAndArgsRegex = makeEngineAndArgsRegex(engineNames);
+
+  const keyValuePairs = queryString.split('&');
+  const queryStringFragments = [];
+
+  let hasResize = false;
+  let optionToResize;
+
+  for (const pair of keyValuePairs) {
+    let m;
+
+    if ((m = pair.match(queryStringEngineAndArgsRegex)) !== null) {
+      const [, engineName, engineArgs = ''] = m;
+      const result = [engineName];
+      const splitChar = engineArgs.includes('+') ? '+' : ',';
+      const remaining = engineArgs.split(splitChar);
+
+      let isEngineOptions = false;
+      let lastSeenOptionIndex = -1;
+      let engineOptions;
+
+      for (const [index, bit] of remaining.entries()) {
+        if (engineName === 'svgfilter' && bit[0] === '-' && bit[1] === '-') {
+          if (!isEngineOptions) {
+            isEngineOptions = true;
+            engineOptions = [];
+          }
+          engineOptions.push(bit.slice(2));
+        } else if (bit[0] === '-') {
+          result.push(bit.slice(1));
+          lastSeenOptionIndex = index + 1; // account for the engine entry
+        } else if (engineName === 'pngquant') {
+          result.push(`speed=${bit}`);
+        } else if (lastSeenOptionIndex > -1) {
+          result[lastSeenOptionIndex] += `=${bit}`;
+        }
+      }
+
+      if (isEngineOptions) {
+        result[0] += `=${engineOptions.join('+')}`;
+      }
+
+      queryStringFragments.push(...result);
+    } else {
+      const keyAndValue = pair.split('=');
+      if (keyAndValue.length === 1) keyAndValue.unshift('');
+      const [op, arg] = keyAndValue;
+
+      if (op === 'setFormat') {
+        let format = arg.toLowerCase();
+        if (format === 'jpg') {
+          format = 'jpeg';
+        }
+        queryStringFragments.push(format);
+      } else if (arg in resizeOptions && !hasResize) {
+        optionToResize = arg;
+      } else {
+        let fragment = pair;
+        if (op === 'resize') {
+          if (arg.indexOf('+') > -1) {
+            // specified using a plus operator
+            fragment = fragment.replace('+', ',');
+          } else if (arg.indexOf(',') === -1) {
+            // single value form of resize
+            fragment += ',';
+          }
+        }
+        queryStringFragments.push(fragment);
+      }
+
+      if (op === 'resize') {
+        if (optionToResize) {
+          queryStringFragments.push(optionToResize);
+          optionToResize = undefined;
+        } else {
+          hasResize = true;
+        }
+      }
+    }
+  }
+
+  return queryStringFragments.join('&');
+}
+
+function parseLegacyQueryString(improInstance, queryString, allowOperation) {
+  const improQueryString = prepareLegacyQueryString(improInstance, queryString);
+  return parseImproQueryString(improInstance, improQueryString, allowOperation);
+}
+
 exports.parseImproQueryString = parseImproQueryString;
+exports.prepareLegacyQueryString = prepareLegacyQueryString;
+exports.parseLegacyQueryString = parseLegacyQueryString;
--- test/queryString.js
+++ test/queryString.js
@@ -155,4 +155,129 @@ describe('queryString', function() {
       );
     });
   });
+
+  describe('prepareLegacyQueryString()', () => {
+    const localExpect = expect.clone();
+
+    localExpect.addAssertion(
+      '<string> when prepared <assertion>',
+      (expect, subject) => {
+        expect.errorMode = 'nested';
+
+        return expect.shift(
+          queryString.prepareLegacyQueryString(impro, subject)
+        );
+      }
+    );
+
+    it('should parse resize (comma separator)', () => {
+      localExpect('resize=800,800', 'when prepared to equal', 'resize=800,800');
+    });
+
+    it('should parse resize (plus separator)', () => {
+      localExpect('resize=800+800', 'when prepared to equal', 'resize=800,800');
+    });
+
+    it('should parse svgfilter with options', () => {
+      localExpect(
+        'svgfilter=--runScript=addBogusElement.js,--bogusElementId=theBogusElementId',
+        'when prepared to equal',
+        'svgfilter=runScript=addBogusElement.js+bogusElementId=theBogusElementId'
+      );
+    });
+
+    it('should parse setFormat and other arguments', () => {
+      localExpect(
+        'setFormat=JPG&resize=800,800',
+        'when prepared to equal',
+        'jpeg&resize=800,800'
+      );
+    });
+
+    it('should parse ignoreAspectRatio followed by resize', () => {
+      localExpect(
+        'ignoreAspectRatio&resize=800,800',
+        'when prepared to equal',
+        'resize=800,800&ignoreAspectRatio'
+      );
+    });
+
+    it('should parse withoutEnlargement followed by resize', () => {
+      localExpect(
+        'withoutEnlargement&resize=800,800',
+        'when prepared to equal',
+        'resize=800,800&withoutEnlargement'
+      );
+    });
+
+    it('should parse resize followed by withoutEnlargement', () => {
+      localExpect(
+        'resize=800,800&withoutEnlargement',
+        'when prepared to equal',
+        'resize=800,800&withoutEnlargement'
+      );
+    });
+
+    it('should parse jpegtran and an argument with -flip', () => {
+      localExpect(
+        'jpegtran=-grayscale,-flip,horizontal',
+        'when prepared to equal',
+        'jpegtran&grayscale&flip=horizontal'
+      );
+    });
+
+    it('should parse optipng with no argument', () => {
+      localExpect('optipng', 'when prepared to equal', 'optipng');
+    });
+
+    it('should parse pngquant with integer argument correctly', () => {
+      localExpect(
+        'resize=800,800&pngquant=8',
+        'when prepared to equal',
+        'resize=800,800&pngquant&speed=8'
+      );
+    });
+
+    it('should parse pngcrush with -rm argument correctly (using -)', () => {
+      localExpect(
+        'resize=800,800&pngcrush=-rem,gAMA',
+        'when prepared to equal',
+        'resize=800,800&pngcrush&rem=gAMA'
+      );
+    });
+
+    it('should parse pngcrush with -rm argument correctly (using +)', () => {
+      localExpect(
+        'resize=800,800&pngcrush=-rem+pHYs',
+        'when prepared to equal',
+        'resize=800,800&pngcrush&rem=pHYs'
+      );
+    });
+
+    it('should parse multiple engines and their operations', () => {
+      localExpect(
+        'resize=800,800&pngquant=8&pngcrush=-rem,gAMA',
+        'when prepared to equal',
+        'resize=800,800&pngquant&speed=8&pngcrush&rem=gAMA'
+      );
+    });
+
+    it('should parse the single format form of resize', () => {
+      localExpect('resize=800', 'when prepared to equal', 'resize=800,');
+    });
+  });
+
+  describe('parseLegacyQueryString()', () => {
+    it('should parse setFormat and other arguments', () => {
+      expect(
+        queryString.parseLegacyQueryString(impro, 'setFormat=png'),
+        'to equal',
+        {
+          operations: [{ name: 'png', args: [] }],
+          leftover: '',
+          consumed: 'png'
+        }
+      );
+    });
+  });
 });
