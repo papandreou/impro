2fb6b001f142342818a3d4ef69babbd5879fdcaa
Alex J Burke <alex@alexjeffburke.com>
2021-05-12 11:46:21 +0200

    Statically define exports in the engines index file.

--- src/engines/index.js
+++ src/engines/index.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-
-fs.readdirSync(__dirname).forEach((fileName) => {
-  module.exports[fileName.replace(/\.js$/, '')] = require('./' + fileName);
-});
+exports.gifsicle = require('./gifsicle');
+exports.gm = require('./gm');
+exports.inkscape = require('./inkscape');
+exports.jpegtran = require('./jpegtran');
+exports.metadata = require('./metadata');
+exports.optipng = require('./optipng');
+exports.pngcrush = require('./pngcrush');
+exports.pngquant = require('./pngquant');
+exports.sharp = require('./sharp');
+exports.svgfilter = require('./svgfilter');
